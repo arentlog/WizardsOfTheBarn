7483b9501abb322ad5ae56e2c80c182bf9f6bd3d
Logan Arent <larent@laneautomotive.com>
2026-03-06 09:11:59 -0500

    Initial navbar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { WizardsComponent } from './pages/wizards/wizards.component';
+import { DataComponent } from './pages/data/data.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'wizards', component: WizardsComponent },
+  { path: 'data', component: DataComponent }
+];
---
+++ src/app/pages/wizards/wizards.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-wizards',
+  standalone: true,
+  imports: [],
+  templateUrl: './wizards.component.html',
+  styleUrl: './wizards.component.css'
+})
+export class WizardsComponent {
+
+}
